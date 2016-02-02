f1a6923058530ec197eb0838ed885ee2adadb6fc
CiprianTalmacel <cprn_talmacel@yahoo.com>
2016-02-02 13:41:10 +0200

    added multiple calls waiting

--- test/simpleFlowTest.js
+++ test/simpleFlowTest.js
@@ -12,14 +12,8 @@ var f = flow.create("Flow example", {
 
     begin:function(a1,a2){
         console.log("Begin");
-        if(a1<a2){
             this.next("step1", "beginToStep1", a1);
             this.next("step2", "beginToStep2", a2);
-        }
-        else {
-            this.next("step2","beginToStep1", a2);
-            this.next("step1","beginToStep2", a1);
-        }
     },
 
     step1:function(a){
@@ -28,31 +22,20 @@ var f = flow.create("Flow example", {
     step2:function(a){
         console.log("Step2 ");
         asyncReturnsTrue(this.continue("step3", "step2ToStep3"));
-        this.next("step4","step2ToStep4",a);
+        asyncReturnsTrue(this.continue("step4", "step2ToStep3"));
+    },
 
+    step3:function(a,b){
+        console.log("Step3");
     },
     step3:function(a,b){
         console.log("Step3");
     },
-    step4:function(a){
-        console.log("Step4")
-    },
-    step5:function(a){
-        console.log("Step5");
-    },
-    step6:function(){
-        console.log("Step6");
-    },
-    send:{
-        join:"step1,step2",
-        code:function(){
-            console.log("Send");
-            this.next("step5","sendToStep5");
-            asyncReturnsTrue(this.continue("step6","sendToStep6"));
-        }
+    step4:function(a,b){
+        console.log("Step4");
     },
     end:{
-        join:'step1,step3,step4,step5,step6',
+        join:'step1,step3,step4',
         code:function(){
             console.log("end step2Arg:");
         }
