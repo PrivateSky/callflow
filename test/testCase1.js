d531c88417ee32e7113688c1578fe802bb24e003
salboaie <master1!23>
2016-02-03 16:23:36 +0200

    refactoring using callback

--- test/testCase1.js
+++ test/testCase1.js
@@ -1,5 +1,5 @@
 var flow = require("../lib/flow.js");
-var assert       = require('semantic-firewall').assert;
+var assert       = require('double-check').assert;
 
 function asyncReturnsTrue(callback){
     setTimeout(function(){
@@ -7,56 +7,64 @@ function asyncReturnsTrue(callback){
     }, 1000);
 }
 
-var logs = "";
-var expectedLogs = "begin" +
-    "step1" +
-    "step2" +
-    "step3" +
-    "step3" +
-    "step4" +
-    "end";
 
-function testResults(){
-    assert.equal(logs,expectedLogs,"problemsAtAsyncCalls");
-}
 
-var f = flow.create("Flow example", {
+assert.callback("Simple flow example", function(end){
+    var logs = "";
+    var expectedLogs = "begin" +
+        "step1" +
+        "step2" +
+        "step3" +
+        "step3" +
+        "step4" +
+        "end";
 
-    begin:function(a1,a2){
-        logs+="begin";
-        asyncReturnsTrue(this.continue("step3", "step2ToStep3"));
-        this.step1();
-    },
-
-    step1:function(a){
-        logs+="step1";
-        this.next("step2","becauseStep2");
-    },
-    step2:function(a){
-        logs+="step2";
-        asyncReturnsTrue(this.continue("step3", "step2ToStep3"));
-        asyncReturnsTrue(this.continue("step4", "step2ToStep3"));
-    },
-
-    step3:function(a,b){
-        logs+="step3";
-    },
-    step4:function(a,b){
-        logs+="step4";
-    },
-    end:{
-        join:'step1,step3,step4',
-        code:function(){
-            logs+="end";
-            testResults();
-        }
-    },
-    error:function(error){
-        if(b){
-            this.result = b;
-        }
+    function testResults(){
+        assert.equal(logs,expectedLogs,"problemsAtAsyncCalls");
+        end();
     }
-});
-f.begin("a1","a2");
+
+
+    var f = flow.create("Flow example", {
+
+        begin:function(a1,a2){
+            logs+="begin";
+            asyncReturnsTrue(this.continue("step3", "step2ToStep3"));
+            this.step1();
+        },
+
+        step1:function(a){
+            logs+="step1";
+            this.next("step2","becauseStep2");
+        },
+        step2:function(a){
+            logs+="step2";
+            asyncReturnsTrue(this.continue("step3", "step2ToStep3"));
+            asyncReturnsTrue(this.continue("step4", "step2ToStep3"));
+        },
+
+        step3:function(a,b){
+            logs+="step3";
+        },
+        step4:function(a,b){
+            logs+="step4";
+        },
+        end:{
+            join:'step1,step3,step4',
+            code:function(){
+                logs+="end";
+                testResults();
+            }
+        },
+        error:function(error){
+            if(b){
+                this.result = b;
+            }
+        }
+    });
+    f.begin("a1","a2");
+
+})
+
 
 
