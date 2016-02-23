1a930c851a6170a0d0d671098b1dffb443a9a064
CiprianTalmacel <cprn_talmacel@yahoo.com>
2016-02-23 17:59:25 +0200

    run joins on next tick... should upgrade all tests

---
+++ test/testJoinRunsOnNextTick.js
@@ -0,0 +1,48 @@
+var flow = require("../lib/flow.js");
+var assert       = require('double-check').assert;
+
+function createDefaultMotivation(){
+
+}
+
+
+assert.callback("Simple test callback join", function(end){
+    var logs = "";
+    var expectedLogs = "begin" +
+        "step1" +
+        "step2" +
+        "step1"+
+        "end";
+
+    function testResults(){
+        assert.equal(logs,expectedLogs,"Difference between expected logs and actual results");
+        end();
+    }
+
+    var f = flow.create("Flow example", {
+        begin:function(a1,a2){
+            logs+="begin";
+            this.step1();
+            this.step2();
+            this.step1();
+        },
+        step1:function(a){
+            logs += "step1";
+
+        },
+        step2:function(a){
+            logs += "step2";
+        },
+        end:{
+            join:"step1,step2",
+            code:function(a){
+                logs += "end";
+                testResults();
+            }
+        }
+    });
+    var fl = f();
+})
+
+
+
