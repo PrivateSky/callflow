dd36de0d2c8f0e23b11a5fd366dc7c9997200d4d
CiprianTalmacel <cprn_talmacel@yahoo.com>
2016-03-12 12:36:43 +0200

    save progress

--- test/testSimpleFlowCallbackWhys.js
+++ test/testSimpleFlowCallbackWhys.js
@@ -2,6 +2,7 @@ var flow = require("../lib/flow.js");
 var assert       = require('double-check').assert;
 var why = require("../../whys/lib/why.js");
 
+
 process.env['RUN_WITH_WHYS'] = true;
 assert.callback("Simple flow-why test", function(end){
     var logs = "";
@@ -13,14 +14,14 @@ assert.callback("Simple flow-why test", function(end){
         var executionSummary = context.getExecutionSummary();
         assert.equal(logs,expectedLogs,"Difference between expected logs and actual results");
         assert.equal(executionSummary.calls.hasOwnProperty('***Starting flow: Flow'),true,"The execution summary does not provide the starting point");
-        assert.equal(executionSummary.calls['***Starting flow: Flow'].calls.hasOwnProperty('begin to step'),true,"The execution summary does not contain call: begin to step");
+        assert.equal(executionSummary.calls['***Starting flow: Flow'].calls.hasOwnProperty('run step'),true,"The execution summary does not contain call: begin to step");
         end();
     }
 
     var f = flow.create("Flow", {
         begin:function(a1,a2){
             logs+="begin";
-            this.step();
+            setTimeout(this.continue("step","run step"),10);
         },
         step:function(a){
             logs += "step";
--- test/testSimpleJoinWhys.js
+++ test/testSimpleJoinWhys.js
@@ -2,6 +2,7 @@ var flow = require("../lib/flow.js");
 var assert       = require('double-check').assert;
 var why = require("../../whys/lib/why.js");
 
+
 process.env['RUN_WITH_WHYS'] = true;
 assert.callback("Simple flow-why test", function(end){
     var logs = "";
@@ -13,19 +14,24 @@ assert.callback("Simple flow-why test", function(end){
         var executionSummary = context.getExecutionSummary();
         assert.equal(logs,expectedLogs,"Difference between expected logs and actual results");
         assert.equal(executionSummary.calls.hasOwnProperty('***Starting flow: Flow'),true,"The execution summary does not provide the starting point");
-        assert.equal(executionSummary.calls['***Starting flow: Flow'].calls.hasOwnProperty('begin to step'),true,"The execution summary does not contain call: begin to step");
+        assert.equal(executionSummary.calls['***Starting flow: Flow'].calls.hasOwnProperty('run step'),true,"The execution summary does not contain call: begin to step");
         end();
     }
 
     var f = flow.create("Flow", {
         begin:function(a1,a2){
             logs+="begin";
-            this.step();
+            this.step.why("run step")()
         },
         step:function(a){
             logs += "step";
-            var context = why.getGlobalCurrentContext();
-            setTimeout(function(){testResults(context)},10);
+        },
+        end:{
+            join:"step",
+            code:function(){
+                var context = why.getGlobalCurrentContext();
+                setTimeout(function(){testResults(context)},10);
+            }
         }
     });
     f();
