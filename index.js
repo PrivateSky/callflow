c3c95aa22e398697d8c94c0d9dd6fd9e9ffe8c3b
salboaie <salboaie@gmail.com>
2018-06-27 16:33:58 +0300

    reverted some wrong deletes

---
+++ index.js
@@ -0,0 +1,137 @@
+
+var path = require("path");
+
+function defaultErrorHandlingImplementation(err, res){
+	//console.log(err.stack);
+	if(err) throw err;
+	return res;
+}
+
+$$ = {
+    errorHandler: {
+        error:function(err, args, msg){
+            console.log(err, "Unknown error from function call with arguments:", args, "Message:", msg);
+        },
+        throwError:function(err, args, msg){
+            console.log(err, "Unknown error from function call with arguments:", args, "Message:", msg);
+            throw err;
+        },
+        ignorePossibleError: function(name){
+            console.log(name);
+        },
+        syntaxError:function(property, swarm, text){
+            //throw new Error("Misspelled member name or other internal error!");
+            var swarmName;
+            try{
+                if(typeof swarm == "string"){
+                    swarmName = swarm;
+                } else
+                if(swarm && swarm.meta){
+                    swarmName  = swarm.meta.swarmTypeName;
+                } else {
+                    swarmName = swarm.getInnerValue().meta.swarmTypeName;
+                }
+            } catch(err){
+                swarmName = err.toString();
+            }
+            if(property){
+                console.log("Wrong member name ", property,  " in swarm ", swarmName);
+                if(text) {
+                    console.log(text);
+                }
+            } else {
+                console.log("Unknown swarm", swarmName);
+            }
+
+        },
+        warning:function(msg){
+            console.log(msg);
+        }
+    },
+    uidGenerator: require("./lib/safe-uuid"),
+    safeErrorHandling:function(callback){
+        if(callback){
+            return callback;
+        } else{
+            return defaultErrorHandlingImplementation;
+        }
+    },
+    __intern:{
+        mkArgs:function(args,pos){
+            var argsArray = [];
+            for(var i = pos; i < args.length; i++){
+                argsArray.push(args[i]);
+            }
+            return argsArray;
+        }
+    },
+    __global:{
+
+    }
+};
+
+$$.defaultErrorHandlingImplementation = defaultErrorHandlingImplementation;
+
+var callflowModule = require("./lib/swarmDescription");
+$$.callflows        = callflowModule.createSwarmEngine("callflow");
+$$.callflow         = $$.callflows;
+$$.flow             = $$.callflows;
+$$.flows            = $$.callflows;
+
+$$.swarms           = callflowModule.createSwarmEngine("swarm", utils);
+$$.swarm            = $$.swarms;
+$$.contracts        = callflowModule.createSwarmEngine("contract", utils);
+$$.contract         = $$.contracts;
+
+$$.PSK_PubSub = require("./lib/soundPubSub");
+
+var loadedModules = {};
+
+$$.__global.__loadLibrayRoot = __dirname + "/../libraries/";
+
+
+
+$$.requireModule = function(name){
+    var existingModule = loadedModules[name];
+    if(!existingModule){
+        var absolutePath = path.resolve( __dirname + "/../modules/" + name);
+        existingModule = require(absolutePath);
+        loadedModules[name] = existingModule;
+    }
+    return existingModule;
+};
+
+$$.securityContext = "system";
+$$.libraryPrefix = "global";
+$$.libraries = {
+    global:{
+
+    }
+};
+
+
+
+$$.loadLibrary = require("./lib/loadLibrary").loadLibrary;
+
+$$.requireLibrary = function(name){
+    var absolutePath = path.resolve(  $$.__global.__loadLibrayRoot + name);
+    return $$.loadLibrary(name,absolutePath);
+};
+
+$$.registerSwarmDescription =  function(libraryName,shortName, description){
+    if(!$$.libraries[libraryName]){
+        $$.libraries[libraryName] = {};
+    }
+    $$.libraries[libraryName][shortName] = description;
+}
+
+var utils = require("./lib/choreographies/utilityFunctions");
+
+
+module.exports = {
+    				createSwarmEngine: require("./lib/swarmDescription").createSwarmEngine,
+                    createJoinPoint: require("./lib/parallelJoinPoint").createJoinPoint,
+                    createSerialJoinPoint: require("./lib/serialJoinPoint").createSerialJoinPoint,
+					"safe-uuid": require("./lib/safe-uuid"),
+					beesHealer: require("./lib/beesHealer")
+				};
