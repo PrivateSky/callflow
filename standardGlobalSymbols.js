f5fc8ce92ef5192357bc69cdb825a535e297b877
Stefan Cosmin Romanescu <stefancosmin.romanescu@gmail.com>
2019-08-27 15:24:10 +0300

    Added JSDoc to $$ to support IDE auto completion

--- standardGlobalSymbols.js
+++ standardGlobalSymbols.js
@@ -4,15 +4,15 @@ if (process.env.NO_LOGS !== 'true') {
     try {
         const PSKLoggerModule = require('psklogger');
         const PSKLogger = PSKLoggerModule.PSKLogger;
-        
+
         logger = PSKLogger.getLogger();
-        
+
         // TODO: remove this once $$ logger is used instead of console
         PSKLoggerModule.overwriteConsole();
 
         console.log('Logger init successful', process.pid);
     } catch (e) {
-        console.warn('Logger not available, using console', e);
+        console.warn('Logger not available, using console');
         logger = console;
     }
 } else {
@@ -30,19 +30,37 @@ $$.registerGlobalSymbol = function (newSymbol, value) {
     }
 };
 
+/**
+ * @method
+ * @name $$#autoThrow
+ * @param {Error} err
+ * @throws {Error}
+ */
+
 $$.registerGlobalSymbol("autoThrow", function (err) {
     if (!err) {
         throw err;
     }
 });
 
-$$.registerGlobalSymbol("propagateError", function (err,callback) {
+/**
+ * @method
+ * @name $$#propagateError
+ * @param {Error} err
+ * @param {function} callback
+ */
+$$.registerGlobalSymbol("propagateError", function (err, callback) {
     if (err) {
         callback(err);
         throw err; //stop execution
     }
 });
 
+/**
+ * @method
+ * @name $$#logError
+ * @param {Error} err
+ */
 $$.registerGlobalSymbol("logError", function (err) {
     if (err) {
         console.log(err);
@@ -50,19 +68,42 @@ $$.registerGlobalSymbol("logError", function (err) {
     }
 });
 
+/**
+ * @method
+ * @name $$#fixMe
+ * @param {...*} args
+ */
 console.log("Fix the fixMe to not display on console but put in logs");
 $$.registerGlobalSymbol("fixMe", function (...args) {
-        //$$.log(...args);
+    //$$.log(...args);
 });
 
+/**
+ * @method - Throws an error
+ * @name $$#exception
+ * @param {string} message
+ * @param {*} type
+ */
 $$.registerGlobalSymbol("exception", function (message, type) {
-        throw new Error(message);
+    throw new Error(message);
 });
 
+/**
+ * @method - Throws an error
+ * @name $$#throw
+ * @param {string} message
+ * @param {*} type
+ */
 $$.registerGlobalSymbol("throw", function (message, type) {
-        throw new Error(message);
+    throw new Error(message);
 });
 
+
+/**
+ * @method - Warns that method is not implemented
+ * @name $$#incomplete
+ * @param {...*} args
+ */
 /* signal a  planned feature but not implemented yet (during development) but
 also it could remain in production and should be flagged asap*/
 $$.incomplete = function (...args) {
@@ -70,8 +111,20 @@ $$.incomplete = function (...args) {
     logger.warn(...args);
 };
 
+/**
+ * @method - Warns that method is not implemented
+ * @name $$#notImplemented
+ * @param {...*} args
+ */
 $$.notImplemented = $$.incomplete;
 
+
+/**
+ * @method Throws if value is false
+ * @name $$#assert
+ * @param {boolean} value - Value to assert against
+ * @param {string} explainWhy - Reason why assert failed (why value is false)
+ */
 /* used during development and when trying to discover elusive errors*/
 $$.registerGlobalSymbol("assert", function (value, explainWhy) {
     if (!value) {
@@ -79,70 +132,126 @@ $$.registerGlobalSymbol("assert", function (value, explainWhy) {
     }
 });
 
+/**
+ * @method
+ * @name $$#flags
+ * @param {string} flagName
+ * @param {*} value
+ */
 /* enable/disabale flags that control psk behaviour*/
 $$.registerGlobalSymbol("flags", function (flagName, value) {
     $$.incomplete("flags handling not implemented");
 });
 
+/**
+ * @method - Warns that a method is obsolete
+ * @name $$#obsolete
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("obsolete", function (...args) {
     args.unshift("Obsolete feature:");
     logger.log(...args);
     console.log(...args);
 });
 
+/**
+ * @method - Uses the logger to log a message of level "log"
+ * @name $$#log
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("log", function (...args) {
     args.unshift("Log:");
     logger.log(...args);
     console.log(...args);
 });
 
+/**
+ * @method - Uses the logger to log a message of level "info"
+ * @name $$#info
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("info", function (...args) {
     args.unshift("Info:");
     logger.log(...args);
     console.log(...args);
 });
 
-
+/**
+ * @method - Uses the logger to log a message of level "error"
+ * @name $$#err
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("err", function (...args) {
     args.unshift("Error:");
     logger.error(...args);
     console.log(...args);
 });
 
+/**
+ * @method - Uses the logger to log a message of level "warning"
+ * @name $$#warn
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("warn", function (...args) {
     args.unshift("Warn:");
     logger.warn(...args);
     console.log(...args);
 });
 
+/**
+ * @method - Uses the logger to log a message of level "syntexError"
+ * @name $$#syntexError
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("syntaxError", function (...args) {
     args.unshift("Unknown syntaxError:");
     logger.log(...args);
 });
 
+/**
+ * @method - Logs an invalid member name for a swarm
+ * @name $$#invalidMemberName
+ * @param {string} name
+ * @param {Object} swarm
+ */
 $$.invalidMemberName = function (name, swarm) {
-    var swarmName = "unknown"
-    if(swarm && swarm.meta){
+    let swarmName = "unknown";
+    if (swarm && swarm.meta) {
         swarmName = swarm.meta.swarmTypeName;
     }
-    var text = "Invalid member name " + name + "in swarm " + swarmName;
+    const text = "Invalid member name " + name + "in swarm " + swarmName;
     console.log(text);
     logger.log(text);
 };
 
-$$.registerGlobalSymbol("invalidSwarmName", function ( swarmName) {
-    var text = "Invalid swarm name " + swarmName;
+/**
+ * @method - Logs an invalid swarm name
+ * @name $$#invalidSwarmName
+ * @param {string} name
+ * @param {Object} swarm
+ */
+$$.registerGlobalSymbol("invalidSwarmName", function (swarmName) {
+    const text = "Invalid swarm name " + swarmName;
     console.log(text);
     logger.log(text);
 });
 
-/* log unknown exceptions*/
+/**
+ * @method - Logs unknown exceptions
+ * @name $$#unknownException
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("unknownException", function (...args) {
     args.unshift("unknownException:");
     logger.log(...args);
 });
 
-/* PrivateSky event, used by monitoring and statistics*/
+/**
+ * @method - PrivateSky event, used by monitoring and statistics
+ * @name $$#event
+ * @param {string} event
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("event", function (event, ...args) {
     if (logger.hasOwnProperty('event')) {
         logger.event(event, ...args);
@@ -151,12 +260,21 @@ $$.registerGlobalSymbol("event", function (event, ...args) {
     }
 });
 
-/* */
-$$.registerGlobalSymbol("redirectLog", function(logType, logObject) {
+/**
+ * @method -
+ * @name $$#redirectLog
+ * @param {string} event
+ * @param {...*} args
+ */
+$$.registerGlobalSymbol("redirectLog", function (logType, logObject) {
     logger.redirect(logType, logObject);
 });
 
-/* log throttling event // it is just an event?*/
+/**
+ * @method - log throttling event // it is just an event?
+ * @name $$#throttlingEvent
+ * @param {...*} args
+ */
 $$.registerGlobalSymbol("throttlingEvent", function (...args) {
     logger.log(...args);
 });
