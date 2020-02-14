83f84850553eb62780473496d06610a413f2d971
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-02-14 14:30:27 +0200

    Changed callflow to require explicit initialisation

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-if($$.callflow){
-    throw new Error("Callflow already initialized!");
-}
+function initialise() {
+    if($$.callflow){
+        throw new Error("Callflow already initialized!");
+    }
 
-function defaultErrorHandlingImplementation(err, res){
-	//console.log(err.stack);
-	if(err) throw err;
-	return res;
-}
+    function defaultErrorHandlingImplementation(err, res){
+        //console.log(err.stack);
+        if(err) throw err;
+        return res;
+    }
 
-$$.__intern = {
+    $$.__intern = {
         mkArgs:function(args,pos){
             var argsArray = [];
             for(var i = pos; i < args.length; i++){
@@ -18,64 +19,65 @@ $$.__intern = {
         }
     };
 
-$$.defaultErrorHandlingImplementation = defaultErrorHandlingImplementation;
+    $$.defaultErrorHandlingImplementation = defaultErrorHandlingImplementation;
 
-var callflowModule = require("./lib/swarmDescription");
-$$.callflows        = callflowModule.createSwarmEngine("callflow");
-$$.callflow         = $$.callflows;
-$$.flow             = $$.callflows;
-$$.flows            = $$.callflows;
-
-
-$$.PSK_PubSub = require("soundpubsub").soundPubSub;
-
-$$.securityContext = null;
-$$.HRN_securityContext = "unnamedSecurityContext"; /*HRN: Human Readable Name */
-$$.libraryPrefix = "global";
-$$.libraries = {
-    global:{
-
-    }
-};
-
-$$.interceptor = require("./lib/InterceptorRegistry").createInterceptorRegistry();
-
-$$.loadLibrary = require("./lib/loadLibrary").loadLibrary;
-
-requireLibrary = function(name){
-    //var absolutePath = path.resolve(  $$.__global.__loadLibraryRoot + name);
-    return $$.loadLibrary(name,name);
-};
-
-require("./constants");
+    var callflowModule = require("./lib/swarmDescription");
+    $$.callflows        = callflowModule.createSwarmEngine("callflow");
+    $$.callflow         = $$.callflows;
+    $$.flow             = $$.callflows;
+    $$.flows            = $$.callflows;
 
 
-$$.pathNormalize = function (pathToNormalize) {
-    const path = require("path");
-    pathToNormalize = path.normalize(pathToNormalize);
+    $$.PSK_PubSub = require("soundpubsub").soundPubSub;
 
-    return pathToNormalize.replace(/[\/\\]/g, path.sep);
-};
+    $$.securityContext = null;
+    $$.HRN_securityContext = "unnamedSecurityContext"; /*HRN: Human Readable Name */
+    $$.libraryPrefix = "global";
+    $$.libraries = {
+        global:{
 
-// add interceptors
+        }
+    };
 
-const crypto = require('crypto');
+    $$.interceptor = require("./lib/InterceptorRegistry").createInterceptorRegistry();
 
-$$.interceptor.register('*', '*', 'before', function () {
-    const swarmTypeName = this.getMetadata('swarmTypeName');
-    const phaseName = this.getMetadata('phaseName');
-    const swarmId = this.getMetadata('swarmId');
-    const executionId = crypto.randomBytes(16).toString('hex');
+    $$.loadLibrary = require("./lib/loadLibrary").loadLibrary;
 
-    this.setMetadata('executionId', executionId);
+    requireLibrary = function(name){
+        //var absolutePath = path.resolve(  $$.__global.__loadLibraryRoot + name);
+        return $$.loadLibrary(name,name);
+    };
 
-    $$.event('swarm.call', {swarmTypeName, phaseName, swarmId});
-});
+    require("./constants");
 
+
+    $$.pathNormalize = function (pathToNormalize) {
+        const path = require("path");
+        pathToNormalize = path.normalize(pathToNormalize);
+
+        return pathToNormalize.replace(/[\/\\]/g, path.sep);
+    };
+
+    // add interceptors
+
+    const crypto = require('crypto');
+
+    $$.interceptor.register('*', '*', 'before', function () {
+        const swarmTypeName = this.getMetadata('swarmTypeName');
+        const phaseName = this.getMetadata('phaseName');
+        const swarmId = this.getMetadata('swarmId');
+        const executionId = crypto.randomBytes(16).toString('hex');
+
+        this.setMetadata('executionId', executionId);
+
+        $$.event('swarm.call', {swarmTypeName, phaseName, swarmId});
+    });
+}
 
 module.exports = {
-    				createSwarmEngine: require("./lib/swarmDescription").createSwarmEngine,
-                    createJoinPoint: require("./lib/parallelJoinPoint").createJoinPoint,
-                    createSerialJoinPoint: require("./lib/serialJoinPoint").createSerialJoinPoint,
-                    createStandardAPIsForSwarms:require("./lib/utilityFunctions/base").createForObject
-				};
+    createSwarmEngine: require("./lib/swarmDescription").createSwarmEngine,
+    createJoinPoint: require("./lib/parallelJoinPoint").createJoinPoint,
+    createSerialJoinPoint: require("./lib/serialJoinPoint").createSerialJoinPoint,
+    createStandardAPIsForSwarms: require("./lib/utilityFunctions/base").createForObject,
+    initialise: initialise
+};
